1e2a634760d2617bab9b6840165fe60b314d697a
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2023-11-30 16:42:17 +0100

    remove jotai provider

--- src/app/providers.tsx
+++
@@ -1,6 +0,0 @@
-import { Provider as JotaiProvider } from "jotai";
-import { ReactNode } from "react";
-
-export default function Providers({ children }: { children: ReactNode }) {
-  return <JotaiProvider>{children}</JotaiProvider>;
-}
